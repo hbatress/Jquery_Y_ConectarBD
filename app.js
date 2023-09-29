067fef1b97326cdf29c117f473cce040d96c968f
BA3 <99779095+hbatress@users.noreply.github.com>
2023-09-29 12:38:39 -0600

    se modifico y realizo la cuarta prueba de conexion, tambien se agrego el codigo de base de datos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 3001; // Puedes cambiar el puerto si lo deseas
+const port = 444; // Puedes cambiar el puerto si lo deseas
 
 // Configura la conexión a la base de datos MySQL
 const db = mysql.createConnection({
